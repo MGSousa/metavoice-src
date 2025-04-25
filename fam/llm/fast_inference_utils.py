FIX inference precision
Signed-off-by: MGSousa <31368750+MGSousa@users.noreply.github.com>

fam/llm/fast_inference_utils.py
@@ -278,7 +278,7 @@ def _load_model(
             k = k.replace(".mlp.c_proj.", ".feed_forward.w2.")
 
     model.load_state_dict(state_dict, assign=True)
-    model = model.to(device=device, dtype=torch.bfloat16)
+    model = model.to(device=device, dtype=precision)
 
     if quantisation_mode == "int8":
         warnings.warn(